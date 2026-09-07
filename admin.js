974118342c5606f34940539a9274126ef57089aa
VARDHANARJUN33 <vardhanarjun33@gmail.com>
2026-09-07 10:24:15 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -27,7 +27,7 @@
    ========================================================= */
 
 const N8N_WEBHOOK_URL =
-    "https://cricwith27.app.n8n.cloud/webhook/hanuman-cars";
+    "https://cricwith27.app.n8n.cloud/webhook-test/hanuman-cars";
 
 
 /* GET CARS WEBHOOK
@@ -36,7 +36,7 @@ const N8N_WEBHOOK_URL =
 */
 
 const CARS_API_URL =
-    "https://cricwith27.app.n8n.cloud/webhook/cars";
+    "https://cricwith27.app.n8n.cloud/webhook-test/cars";
 
 
 const ADMIN_KEYS = [
